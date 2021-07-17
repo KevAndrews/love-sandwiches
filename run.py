5c328a12f08a691d4534d35e3d1f96934849b315
KevAndrews <nivek87@gmail.com>
2021-07-17 20:29:10 +0100

    Add get_stock_values Return the Stock value for each Sandwich

--- run.py
+++ run.py
@@ -122,6 +122,18 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
-print("Welcome to Love Sandwiches Data Automation")
-main()
+def get_stock_values(data):
+    """
+    Return the Stock value for each Sandwich
+    """
+    headings = SHEET.worksheet("stock").row_values(1)
+
+    return {headings[i] : data[i] for i in range(len(headings))}#dict(zip(headings, data))
+
+print("Welcome to Love Sandwiches Data Automation.\n")
+stock_data = main()
+
+stock_values = get_stock_values(stock_data)
+print(stock_values)
